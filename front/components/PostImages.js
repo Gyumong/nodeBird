ed832a57fc7fa826b804f13a7a051b34a66468bc
Gyumong <rlaalsrb3559@naver.com>
2020-12-10 19:22:13 +0900

    이미지 구현 -slick 사용해서 넘기기도함

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -1,5 +1,71 @@
+import { PlusOutlined } from "@ant-design/icons";
+import React, { useState, useCallback } from "react";
+import ImagesZoom from "./ImagesZoom";
+
 const PostImages = ({ images }) => {
-  return <div>이미지</div>;
+  const [showImageZoom, setShowImageZoom] = useState(false);
+  const onZoom = useCallback(() => {
+    setShowImageZoom(true);
+  }, []);
+
+  const onClose = useCallback(() => {
+    setShowImageZoom(false);
+  }, []);
+  if (images.length === 1) {
+    return (
+      <>
+        <img
+          src={images[0].src}
+          alt={images[0].src}
+          style={{ width: "50%", display: "inline-block" }}
+          onClick={onZoom}
+        />
+        {showImageZoom && <ImagesZoom images={images} onClose={onClose} />}
+      </>
+    );
+  }
+  if (images.length === 2) {
+    return (
+      <>
+        <img
+          src={images[0].src}
+          alt={images[0].src}
+          style={{ width: "50%", display: "inline-block" }}
+          onClick={onZoom}
+        />
+        <img
+          src={images[1].src}
+          alt={images[1].src}
+          style={{ width: "50%", display: "inline-block" }}
+          onClick={onZoom}
+        />
+        {showImageZoom && <ImagesZoom images={images} onClose={onClose} />}
+      </>
+    );
+  }
+  return (
+    <>
+      <img
+        src={images[0].src}
+        alt={images[0].src}
+        style={{ width: "50%", display: "inline-block" }}
+        onClick={onZoom}
+      />
+      <div
+        style={{
+          display: "inline-block",
+          width: "50%",
+          textAlign: "center",
+          verticalAlign: "middle",
+        }}
+      >
+        <PlusOutlined />
+        <br />
+        {images.length - 1} 개의 사진 더보기
+      </div>
+      {showImageZoom && <ImagesZoom images={images} onClose={onClose} />}
+    </>
+  );
 };
 
 export default PostImages;
